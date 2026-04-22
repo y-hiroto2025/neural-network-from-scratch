f6da507cb5063b721c91f00c1dfc6058768a38fa
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-04-22 21:44:45 +0900

    implement backward pass of Affine and ReLU

--- src/activations.py
+++ src/activations.py
@@ -6,8 +6,18 @@ class ReLU:
   入力データが0以下ならば0、そうでないならそのままの値を出力するReLU層
   """
   def __init__(self):
-    self.x = None
+    self.mask = None        # 順伝播の時に0以下だった箇所の記憶
 
   def forward(self, x):
-    output = np.maximum(0, x)
+    self.mask = (x <= 0)    # 0以下の場所をFalse
+
+    output = x.copy()
+    output[self.mask] = 0   # 0以下の場所を0
+
     return output
+
+  def backward(self, dout):
+    dx = dout.copy()
+    dx[self.mask] = 0       # 順伝播で0以下だった箇所を0
+
+    return dx
--- src/layers.py
+++ src/layers.py
@@ -8,11 +8,23 @@ class Affine:
   def __init__(self, W, b):
     # W shape       : (input dim, output dim)
     # b shape       : (output dim, )
-    self.W = W 
-    self.b = b
+    self.W = W      # weight
+    self.b = b      # bias
+    self.x = None   # forwardの入力データ記憶
+    self.dW = None  # weightの勾配
+    self.db = None  # biasの勾配
 
-  def forword(self, x):
+  def forward(self, x):
     # x shape       : (batch size, input dim)
     # output shape  : (batth size, output dim)
+    self.x = x
     output = np.dot(x, self.W) + self.b
+
     return output
+
+  def backward(self, dout):
+    dx = np.dot(dout, self.W.T)
+    self.dW = np.dot(self.x.T, dout)
+    self.db = np.sum(dout, axis=0)
+
+    return dx
