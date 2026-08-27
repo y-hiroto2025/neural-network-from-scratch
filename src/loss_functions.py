36ff5a283a0dc92ed942edae891054e7057e1039
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2026-08-27 13:08:14 +0900

    feat: simplenet.py

--- src/loss_functions.py
+++ src/loss_functions.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def sum_squared_error(y, t):
     return 0.5 * np.sum((y-t)**2)
 
---
+++ src/simplenet.py
@@ -0,0 +1,27 @@
+import numpy as np
+from activation_functions import softmax
+from loss_functions import cross_entropy_error
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2, 3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
+
+net = simpleNet()
+print(net.W)
+
+x = np.array([0.6, 0.9])
+p = net.predict(x)
+print(p, np.argmax(p))
+
+t = np.array([0, 0, 1])
+print(net.loss(x, t))
